449db13b6c450fc8182c6e332aa4a686793358cc
Samson Wayne <smuhadia4@gmail.com>
2023-08-19 02:43:20 -0700

    corrected script 13

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for char in range(ord('a'), ord('z') + 1):
-    if chr(char) != 'q' and chr(char) != 'e':
-        print(chr(char), end='')
+for i in range(ord('a'), ord('z') + 1):
+    if i != ord('q') and i != ord('e'):
+        print(chr(i), end='')
