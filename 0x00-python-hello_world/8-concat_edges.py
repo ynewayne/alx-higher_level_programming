049579b95872bd420439a1481f1d22694c8a5948
Samson Wayne <smuhadia4@gmail.com>
2023-08-19 02:28:30 -0700

    corrected script 8

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 str1 = "object"
-str2 = "-oriented"
+str2 = "oriented"
 str3 = " programming"
 str4 = " with Python"
-print(str1 + str2[1:] + str3 + str4)
+print(str1 + "-" + str2 + str3 + str4)
