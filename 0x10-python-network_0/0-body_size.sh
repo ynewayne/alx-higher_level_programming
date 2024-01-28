a4542cf48aa9ac6c3ce61a47b28279fc319eedfc
Samson Wayne <smuhadia4@gmail.com>
2024-01-28 03:39:12 -0800

    made 1-body.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 # This script takes a URL, sends a request using curl, and displays the size of the response body in bytes
-# The URL is provided as a command-line argument
-
 curl -sI "$1" | grep -i Content-Length | awk '{print $2}'
