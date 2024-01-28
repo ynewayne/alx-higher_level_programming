b397bdab6093e3dc624e63dbabdd021d61bd7861
Samson Wayne <smuhadia4@gmail.com>
2024-01-28 03:40:46 -0800

    corrected the code

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# This script sends a GET request to a URL using curl and displays the body of the response for a 200 status code
-
+# This script sends a GET request to a URL using curl and displays the body of the response
 curl -sI "$1" | grep -i "HTTP/1.1 200 OK" && curl -s "$1"
